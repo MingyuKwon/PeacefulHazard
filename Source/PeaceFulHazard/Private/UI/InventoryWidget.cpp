2d029fdd1daa140be5120ae1ea828b45510aaed5
Kwon Mingyu <kmgmg230601@gmail.com>
2024-08-21 20:45:59 +0900

    make all image to button

--- Source/PeaceFulHazard/Private/UI/InventoryWidget.cpp
+++ Source/PeaceFulHazard/Private/UI/InventoryWidget.cpp
@@ -2,4 +2,33 @@
 
 
 #include "UI/InventoryWidget.h"
+#include "Components/Image.h"
+#include "Components/Button.h"
+
+void UInventoryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ItemButton1)
+	{
+		ItemButton1->OnClicked.AddDynamic(this, &UInventoryWidget::OnItemButtonClicked);
+		ItemButton1->OnHovered.AddDynamic(this, &UInventoryWidget::OnItemButtonHovered);
+		ItemButton1->OnUnhovered.AddDynamic(this, &UInventoryWidget::OnItemButtonUnhovered);
+	}
+}
+
+void UInventoryWidget::OnItemButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("ItemButton1 clicked!"));
+}
+
+void UInventoryWidget::OnItemButtonHovered()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Mouse hovered over ItemButton1!"));
+}
+
+void UInventoryWidget::OnItemButtonUnhovered()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Mouse unhovered from ItemButton1!"));
+}
 
--- Source/PeaceFulHazard/Public/UI/InventoryWidget.h
+++ Source/PeaceFulHazard/Public/UI/InventoryWidget.h
@@ -1,17 +1,120 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "InventoryWidget.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UTextBlock;
+
 UCLASS()
 class PEACEFULHAZARD_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void NativeConstruct() override;
+
+	//////////////////////////////////////// Button Start//////////////////////////////////////////////////////////
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton1;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton2;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton3;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton4;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton5;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton6;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton7;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton8;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton9;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton10;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton11;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton12;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton13;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton14;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ItemButton15;
+
+	UFUNCTION()
+	void OnItemButtonClicked();
+
+	UFUNCTION()
+	void OnItemButtonHovered();
+
+	UFUNCTION()
+	void OnItemButtonUnhovered();
+
+	//////////////////////////////////////// Button End //////////////////////////////////////////////////////////
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText1;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText2;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText3;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText4;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText5;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText6;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText7;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText8;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText9;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText10;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText11;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText12;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText13;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ItemCountText14;
+
+
+
 };
